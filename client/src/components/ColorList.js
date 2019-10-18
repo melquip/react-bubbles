a2e1e6fc1ca47074822d630c5a28b73e4ee66cbf
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-18 10:07:31 +0100

    Extra: Added form validation

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -9,37 +9,71 @@ const initialColor = {
 const ColorList = ({ colors, updateColors }) => {
 	const [editing, setEditing] = useState(false);
 	const [colorToEdit, setColorToEdit] = useState(initialColor);
+	const [colorFormErrors, setColorFormErrors] = useState([]);
 
 	const editColor = color => {
 		setEditing(true);
 		setColorToEdit(color);
+		setColorFormErrors([]);
 	};
+
+	const isValidColorForm = (currentColor) => {
+		let isValid = true;
+		let errors = [];
+		let duplicateColor;
+		let duplicateColorHex;
+		if (typeof currentColor === 'undefined') {
+			currentColor = { ...colorToEdit };
+		}
+		if (typeof currentColor.id === 'undefined') {
+			duplicateColor = colors.find(color => currentColor.color === color.color);
+			duplicateColorHex = colors.find(color => currentColor.code.hex === color.code.hex);
+		} else if (currentColor.id > 0) {
+			duplicateColor = colors.find(color => currentColor.color === color.color &&
+				color.id !== currentColor.id);
+			duplicateColorHex = colors.find(color => currentColor.code.hex === color.code.hex &&
+				color.id !== currentColor.id);
+		}
+		if (typeof duplicateColor !== 'undefined') {
+			isValid = false;
+			errors.push('There\'s already a color with this name in the list!');
+		}
+		if (typeof duplicateColorHex !== 'undefined') {
+			isValid = false;
+			errors.push('There\'s already a color with this hex code in the list!');
+		}
+		setColorFormErrors(errors);
+		console.log('errors', errors, currentColor, duplicateColor);
+		return isValid;
+	}
 
 	const saveEdit = e => {
 		e.preventDefault();
 		// Make a put request to save your updated color
 		// think about where will you get the id from...
 		// where is is saved right now?
-		if (colorToEdit.id > 0) {
-			axiosWithAuth().put(`colors/${colorToEdit.id}`, colorToEdit)
-				.then(response => {
-					const editedColor = response.data;
-					updateColors(colors.map(color => {
-						if (color.id === editedColor.id) return editedColor;
-						return color;
-					}))
-					setColorToEdit(initialColor);
-					setEditing(false);
-				})
-				.catch(err => console.log(err));
-		} else {
-			axiosWithAuth().post(`colors`, colorToEdit)
-				.then(response => {
-					updateColors(response.data);
-					setColorToEdit(initialColor);
-					setEditing(false);
-				})
-				.catch(err => console.log(err));
+		if (isValidColorForm()) {
+			if (colorToEdit.id > 0) {
+				axiosWithAuth().put(`colors/${colorToEdit.id}`, colorToEdit)
+					.then(response => {
+						const editedColor = response.data;
+						updateColors(colors.map(color => {
+							if (color.id === editedColor.id) return editedColor;
+							return color;
+						}))
+						setColorToEdit(initialColor);
+						setEditing(false);
+					})
+					.catch(err => console.log(err));
+			} else {
+				axiosWithAuth().post(`colors`, colorToEdit)
+					.then(response => {
+						updateColors(response.data);
+						setColorToEdit(initialColor);
+						setEditing(false);
+					})
+					.catch(err => console.log(err));
+			}
 		}
 	};
 
@@ -55,6 +89,24 @@ const ColorList = ({ colors, updateColors }) => {
 	const newColor = () => {
 		setEditing(true);
 		setColorToEdit(initialColor);
+		setColorFormErrors([]);
+	}
+
+	const onInputChange = (value, type) => {
+		let state;
+		if (type === 1) {
+			state = {
+				...colorToEdit,
+				color: value
+			};
+		} else {
+			state = {
+				...colorToEdit,
+				code: { hex: value }
+			};
+		}
+		setColorToEdit(state);
+		isValidColorForm(state);
 	}
 
 	return (
@@ -88,25 +140,22 @@ const ColorList = ({ colors, updateColors }) => {
 					<legend>{colorToEdit.id > 0 ? 'edit' : 'new'} color</legend>
 					<label>
 						color name:
-            <input
-							onChange={e =>
-								setColorToEdit({ ...colorToEdit, color: e.target.value })
-							}
+            			<input
+							onChange={e => onInputChange(e.target.value, 1)}
 							value={colorToEdit.color}
 						/>
 					</label>
 					<label>
 						hex code:
-            <input
-							onChange={e =>
-								setColorToEdit({
-									...colorToEdit,
-									code: { hex: e.target.value }
-								})
-							}
+            			<input
+							onChange={e => onInputChange(e.target.value, 2)}
 							value={colorToEdit.code.hex}
 						/>
 					</label>
+					<label>
+						<div className="color-box" style={{ backgroundColor: colorToEdit.code.hex }}></div>
+					</label>
+					{colorFormErrors.length > 0 ? colorFormErrors.map(err => <p key={err}>{err}</p>) : null}
 					<div className="button-row">
 						<button type="submit">save</button>
 						<button onClick={() => setEditing(false)}>cancel</button>
